414cb9b29b9f31d74df55db2472e56b9f5a1dd2f
davidswygart <davidswygart@gmail.com>
2024-08-26 15:54:05 -0400

    Get rid of uneeded files

--- recordVideo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#This is a simple wrapper that calls the python script capture_video.py
-#Arguments: $1 = Length (seconds)
-
-cd "$(dirname "$0")" #CD to directory of this script
-
-v4l2-ctl --set-ctrl wide_dynamic_range=1 -d /dev/v4l-subdev0 #Turn on HDR video mode
-python recordVideo.py ${1:-0} 
